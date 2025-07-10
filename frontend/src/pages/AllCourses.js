cd2fcda09c6f18533401a4d8d9a58b3f9783289f
Pranish97 <99318459+Pranish97@users.noreply.github.com>
2025-07-10 22:18:02 +0545

    Completed Design for Upload Course Section add Api to add Course

--- frontend/src/pages/AllCourses.js
+++ frontend/src/pages/AllCourses.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdAdd } from "react-icons/md";
+import AddCourse from "../components/AddCourse";
 
 const AllCourses = () => {
-  return <div>AllCourses</div>;
+  const [openAddSection, setOpenAddSection] = useState(false);
+  return (
+    <div>
+      <div className="flex justify-between items-center px-20 py-2">
+        <h2 className="text-bold text-lg">All Courses</h2>
+        <button
+          className="bg-navigation px-4 py-2 text-white rounded hover:scale-105 flex items-center"
+          onClick={() => setOpenAddSection(true)}
+        >
+          <MdAdd className="pr-1 text-2xl" />
+          Add Courses
+        </button>
+      </div>
+
+      {openAddSection && <AddCourse onClose={() => setOpenAddSection(false)} />}
+    </div>
+  );
 };
 
 export default AllCourses;
